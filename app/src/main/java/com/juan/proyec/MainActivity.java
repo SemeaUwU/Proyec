2d14b92c759d7045add57056599c035718076884
SemeaUwU <113060652+SemeaUwU@users.noreply.github.com>
2022-09-11 21:30:12 -0300

    a

--- app/src/main/java/com/juan/proyec/MainActivity.java
+++ app/src/main/java/com/juan/proyec/MainActivity.java
@@ -2,13 +2,35 @@ package com.juan.proyec;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email,password;
+    Button btn_i,btn_r;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.Email);
+        password = findViewById(R.id.Password);
+
+        btn_i = findViewById(R.id.btn_i);
+        btn_r = findViewById(R.id.btn_r);
+
+        btn_r.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/juan/proyec/MainActivity2.java
+++ app/src/main/java/com/juan/proyec/MainActivity2.java
@@ -3,12 +3,23 @@ package com.juan.proyec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button btn_reg;
+    EditText email_r,password_r;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        email_r = findViewById(R.id.Email_r);
+        password_r = findViewById(R.id.Password_r);
+        btn_reg = findViewById(R.id.btn_reg);
+
+
     }
 }
